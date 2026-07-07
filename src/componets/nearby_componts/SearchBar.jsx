133d8d0857538cbb1b37b4004bef2f4140fda051
Veera <veerakuraganti3@gmail.com>
2026-07-07 09:44:24 +0530

    fix:search componts

--- src/componets/nearby_componts/SearchBar.jsx
+++ src/componets/nearby_componts/SearchBar.jsx
@@ -1,9 +1,22 @@
 import './SearchBar.css';
 
-export function SearchBar(){
-    return(
+import DetectLogo from '../../assets/icons/detect-location.svg';
+import SearchIcon from '../../assets/icons/search-black.svg'
+
+export function SearchBar() {
+    return (
         <>
-        <input type="text" />
+            <div className='search-bar'>
+                <span className='detect'>
+                    <img src={SearchIcon} alt="serach" className='detect-search-icon' width={30} height={30}/>
+                    <input type="text" placeholder='search for your location' className='input-location' />
+                    <div className='auto-location-detect'>
+                        <img src={DetectLogo} alt="detect-location-logo" width={15} height={15}/>
+                         <p>Detect location</p>
+                    </div>             
+                </span>
+            </div>
+
         </>
     );
 }
