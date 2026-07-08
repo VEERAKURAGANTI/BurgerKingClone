6aa2ffe705606636d79bea4ccaa5be5d5926866e
Veera <veerakuraganti3@gmail.com>
2026-07-08 23:55:57 +0530

    fact:oggle componet

--- src/componets/home_componets/Toggle.jsx
+++ src/componets/home_componets/Toggle.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Toggle.css';
- // Dine-in/Takeaway
+// Dine-in/Takeaway
 export function Toggle() {
   const [isDineIn, setIsDineIn] = useState(true);
 
@@ -11,10 +11,8 @@ export function Toggle() {
   return (
     <div className="toggle-container">
       <span className={`label ${!isDineIn ? 'active' : 'inactive'}`}>DELIVERY</span>
-      <div
-        className={`switch ${isDineIn ? 'active' : ''}`}
-        onClick={handleToggle}
-      >
+        <div className={`switch ${isDineIn ? 'active' : ''}`}
+             onClick={handleToggle}>
         <div className="slider"></div>
       </div>
       <span className={`label ${isDineIn ? 'active' : 'inactive'}`}>DINE-IN/TAKEAWAY</span>
