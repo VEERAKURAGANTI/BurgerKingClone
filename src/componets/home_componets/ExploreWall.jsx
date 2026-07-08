241699aa4ea0efd9b4286c9bec5ff77944d37d9d
Veera <veerakuraganti3@gmail.com>
2026-07-08 23:49:46 +0530

    fix: content inside ExploreWall componet

--- src/componets/home_componets/ExploreWall.jsx
+++ src/componets/home_componets/ExploreWall.jsx
@@ -1,16 +1,18 @@
-import {NavLink} from 'react-router-dom' ;
+import { NavLink } from 'react-router-dom';
 import ExploreWallImg from '../../assets/exploreTheBKWall.jpg';
 
 import './ExploreWall.css';
 
-export function ExploreWall(){
-  return(
+export function ExploreWall() {
+  return (
     <div className="container">
-     <NavLink className="menu-btn"><button>explore full menu</button></NavLink>
-     <NavLink className="explore-wall">
+      <NavLink className="menu-btn"><button>explore full menu</button></NavLink>
+      <NavLink className="explore-wall">
         <img src={ExploreWallImg} alt="explore-wall" />
-     </NavLink>
-    <div className='copy-right'><p><b>Disclamer :</b> All Images used are illustrative and strictly for representational purposes only</p></div>
+      </NavLink>
+      <div className='copy-right'>
+        <p><b>Disclamer :</b> All Images used are illustrative and strictly for representational purposes only</p>
+      </div>
     </div>
   );
 }
