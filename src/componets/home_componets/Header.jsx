220300af572fd5f0718597914e1ee9a2dbf61b66
Veera <veerakuraganti3@gmail.com>
2026-07-08 23:45:30 +0530

    fix:Nav link icon flip style

--- src/componets/home_componets/Header.jsx
+++ src/componets/home_componets/Header.jsx
@@ -2,9 +2,16 @@ import "./Header.css";
 import logo from '../../assets/logo.png';
 import { NavLink } from 'react-router-dom';
 import store from '../../assets/icons/store-icon.svg';
+import storeActive from '../../assets/icons/dine-in-br.svg';
+
 import deals from '../../assets/icons/deals-icon.svg';
+import dealsActive from '../../assets/icons/offers-icon.svg';
+
 import user from '../../assets/icons/user-icon.svg';
+import userActive from '../../assets/icons/login-icon.svg';
+
 import cart from '../../assets/icons/cart-icon.svg';
+import cartActive from '../../assets/icons/cart-active.svg';
 import search from '../../assets/icons/search-icon.svg';
 import location from '../../assets/location.svg';
 
@@ -16,33 +23,63 @@ export function Header() {
             <div className="header">
                 <div className="user-choice" >
                     <NavLink className="logo" to="/">
-                       <img src={logo} alt="logo" className="img-logo" width={79} height={79} />
+                        <img src={logo} alt="logo" className="img-logo" width={79} height={79} />
                     </NavLink>
                     <Toggle />
                     <div className="header_devider"></div>
                     <NavLink className="user-input" to="/search-location">
                         <input type="text" placeholder="location" className="search-location" />
-                        <img src={location} alt="search-icon" width={10} height={20} className="location-icon"/>
+                        <img src={location} alt="search-icon" width={10} height={20} className="location-icon" />
                     </NavLink>
                 </div>
 
-            
+
                 <nav className="nav-links">
-                    <NavLink className="nav-pages" to="/nearby-store">
-                        <img src={store} alt="store-icon" className="store" /> NEARBY STORE
+                    <NavLink to="/nearby-store" className={({ isActive }) => isActive ? "nav-pages active-link" : "nav-pages"}>
+                        {({ isActive }) => (
+                            <>
+                                <img
+                                    src={isActive ? storeActive : store}
+                                    alt="store-icon"
+                                />
+                                NEARBY STORE
+                            </>
+                        )}
                     </NavLink>
-                    <NavLink className="nav-pages" to="/king-deals">
-                        <img src={deals} alt="king-deals-icon" /> KING DEALS
+                    <NavLink to="/king-deals" className={({ isActive }) => isActive ? "nav-pages active-link" : "nav-pages"}>
+                        {({ isActive }) => (
+                            <>
+                                <img
+                                    src={isActive ? dealsActive : deals}
+                                    alt="king-deals-icon" />
+                                    KING DEALS
+                            </>
+                        )}
+
                     </NavLink>
-                    <NavLink className="nav-pages" to="/my-profile">
-                        <img src={user} alt="profile-icon" /> MY PROFILE
+                    <NavLink to="/my-profile" className={({ isActive }) => isActive ? "nav-pages active-link" : "nav-pages"}>
+                        {({ isActive }) => (
+                            <>
+                                <img
+                                    src={isActive ? userActive : user} 
+                                    alt="profile-icon" /> 
+                                    MY PROFILE
+                            </>
+                        )}
                     </NavLink>
-                    <NavLink className="nav-pages" to="/cart">
-                        <img src={cart} alt="cart-icon" /> CART
+                    <NavLink to="/cart" className={({ isActive }) => isActive ? "nav-pages active-link" : "nav-pages"}>
+                        {({ isActive }) => (
+                            <>
+                                <img
+                                    src={isActive? cartActive:cart}
+                                    alt="cart-icon" />
+                                    CART
+                            </>
+                        )}
                     </NavLink>
                 </nav>
-            
-                <NavLink className="search-icon" to="/search">
+
+                <NavLink className="search-icon" to="/search" >
                     <img src={search} alt="search-icon" />
                 </NavLink>
 
